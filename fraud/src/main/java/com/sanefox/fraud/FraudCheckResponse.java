263da00fdf098e2e7c1e38aa29d761b077d418df
Sanefoxx <alexndr.lisitsin@gmail.com>
2022-03-29 12:07:00 +0300

    created new classes of FraudCheck and changed records to classes with lombok

---
+++ fraud/src/main/java/com/sanefox/fraud/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.sanefox.fraud;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
---
+++ fraud/src/main/java/com/sanefox/fraud/FraudController.java
@@ -0,0 +1,19 @@
+package com.sanefox.fraud;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/v1/fraud-check")
+public class FraudController {
+
+    private FraudCheckService fraudCheckService;
+
+    @GetMapping(path="{customerId}")
+    public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
+        boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+
+        return new FraudCheckResponse(isFraudulentCustomer);
+    }
+}
